37a2ef81574f277561232a7dc2bf2d8ef89fec20
weat0212 <weat0212@gmail.com>
2020-11-06 14:13:43 +0800

    Initial Project Add

---
+++ src/main/java/com/example/petclinicspring/PetclinicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinicspring/PetclinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
