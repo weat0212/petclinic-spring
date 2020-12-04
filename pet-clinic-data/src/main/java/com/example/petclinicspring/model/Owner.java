a8c235ae2b9340a17dd9f0bf1dc1b0369835c7c0
weat0212 <weat0212@gmail.com>
2020-12-04 17:54:31 +0800

    Added visits, updated with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/com/example/petclinicspring/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Owner.java
@@ -1,5 +1,7 @@
 package com.example.petclinicspring.model;
 
+import java.util.Set;
+
 /**
  * @author weat0212@gmail.com
  * @project petclinic-spring
@@ -7,4 +9,14 @@ package com.example.petclinicspring.model;
  * @date 2020/11/14 下午 02:35
  */
 public class Owner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
