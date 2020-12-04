c2a8ba930815aefd87407c0cda9f26d6f4a67af9
weat0212 <weat0212@gmail.com>
2020-12-04 18:05:36 +0800

    Added Specialties, added to vet. Closes #27

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Specialty.java
@@ -0,0 +1,20 @@
+package com.example.petclinicspring.model;
+
+/**
+ * @author weat0212@gmail.com
+ * @project petclinic-spring
+ * @package com.example.petclinicspring.model
+ * @date 2020/12/4 下午 05:56
+ */
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/example/petclinicspring/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/model/Vet.java
@@ -1,5 +1,7 @@
 package com.example.petclinicspring.model;
 
+import java.util.Set;
+
 /**
  * @author weat0212@gmail.com
  * @project petclinic-spring
@@ -7,4 +9,14 @@ package com.example.petclinicspring.model;
  * @date 2020/11/14 下午 02:35
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
