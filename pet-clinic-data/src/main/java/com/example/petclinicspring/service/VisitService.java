01f80dccec4d7854d0fd7652eb2d0b9e2f307842
weat0212 <weat0212@gmail.com>
2020-12-16 13:19:23 +0800

    Implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/service/VisitService.java
@@ -0,0 +1,10 @@
+package com.example.petclinicspring.service;
+
+import com.example.petclinicspring.model.Visit;
+
+/**
+ * @author I-Chung, Wang
+ * @date 2020/12/16 下午 01:06
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinicspring/service/map/VisitMapService.java
@@ -0,0 +1,45 @@
+package com.example.petclinicspring.service.map;
+
+import com.example.petclinicspring.model.Visit;
+import com.example.petclinicspring.service.VisitService;
+
+import java.util.Set;
+
+/**
+ * @author I-Chung, Wang
+ * @date 2020/12/16 下午 01:07
+ */
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+             || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
